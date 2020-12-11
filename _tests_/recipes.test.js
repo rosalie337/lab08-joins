578a5ad598128d17363fa9ca0032db72c0c042e1
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-12-10 23:03:30 -0800

    changes events model

--- _tests_/recipes.test.js
+++ _tests_/recipes.test.js
@@ -16,7 +16,6 @@ describe('recipes routes', () => {
     const response = await request(app)
       .post('/api/v1/recipe')
       .send({
-        recipe_photo: 'food.png',
         ingredients:`3 tablespoons butter plus 1 tablespoon for tossing the pasta,
             ½ cup chopped onion,
             ⅔ cup chopped celery,
@@ -40,7 +39,6 @@ describe('recipes routes', () => {
 
     expect(response.body).toEqual({
       id: '1',
-      recipe_photo: 'food.png',
       ingredients:`3 tablespoons butter plus 1 tablespoon for tossing the pasta,
           ½ cup chopped onion,
           ⅔ cup chopped celery,
@@ -58,6 +56,7 @@ describe('recipes routes', () => {
       Add ground beef, a large pinch of salt and a few grindings of pepper. Crumble the meat with a fork, stir well and cook until the beef has lost its raw, red color.
       Add milk and let it simmer gently, stirring frequently, until it has bubbled away completely. Add a tiny grating -- about 1/8 teaspoon -- of nutmeg, and stir.
       Add the wine, let it simmer until it has evaporated, then add the tomatoes and stir thoroughly to coat all ingredients well. When the tomatoes begin to bubble, turn the heat down so that the sauce cooks at the laziest of simmers, with just an intermittent bubble breaking through to the surface. Cook, uncovered, for 3 hours or more, stirring from time to time. While the sauce is cooking, you are likely to find that it begins to dry out and the fat separates from the meat. To keep it from sticking, add 1/2 cup of water whenever necessary. At the end, however, no water at all must be left and the fat must separate from the sauce. Taste and correct for salt.
-      Toss with cooked drained pasta, adding the tablespoon of butter, and serve with freshly grated Parmesan on the side.` });
+      Toss with cooked drained pasta, adding the tablespoon of butter, and serve with freshly grated Parmesan on the side.` 
+    });
   });
-});
+}); 
